34a5a3873809060e0a7d8e68037cb71456a3b1e9
mi <whymi627@gmil.com>
2024-03-07 16:57:02 +0900

    d0307 finish

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -36,6 +36,9 @@ function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
     socket.send(makeMessage("new_message", input.value));
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
     input.value = "";
 }
 function handleNickSubmit(event) {
