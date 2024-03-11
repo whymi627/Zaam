55f6ca54a6dbac3b3ec7c5b9fc683af80a865a65
mi <whymi627@gmil.com>
2024-03-11 10:37:07 +0900

    1_showroom(modified)

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,21 +1,65 @@
 //front-end
 
+// 자동적으로 backend socket.io와 연결해주는 함수
 const socket = io();
 
-const welcom = document.getElementById("welcome");
-const form = document.querySelector("form");
+const welcome = document.getElementById("welcome");
+const form = welcome.querySelector("form");
 const room = document.getElementById("room");
 
 room.hidden = true;
 
-function backendDone(msg) {
-    console.log(`The backend says: `, msg);
+let roomName;
+
+function addMessage(message){
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+
+}
+
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input = room.querySelector("input");
+    const value = input.value;
+    socket.emit("new_message", input.value, roomName, () => {
+        addMessage(`You: ${value}`);
+    });
+    input.value="";
+
+}
+
+
+function showRoom() {
+   welcome.hidden = true;
+   room.hidden = false;
+   const h3 = room.querySelector("h3");
+   h3.innerText = `Room ${roomName}`;
+   const form = room.querySelector("form");
+   form.addEventListener("submit", handleMessageSubmit);
+
 }
 
 function handleRoomSubmit(event){
     event.preventDefault();
     const input = form.querySelector("input");
-    socket.emit("enter_room", { payload: input.value}, backendDone);
+    socket.emit("enter_room", input.value, showRoom);
+    roomName = input.value;
     input.value ="";
 }
+
+
 form.addEventListener("submit", handleRoomSubmit);
+
+socket.on("welcome", () => {
+    console.log("Welcome event received!");
+    addMessage("Someone Joined!!.");
+});
+
+socket.on("bye", () => {
+    console.log("bye event received!");
+    addMessage("Someone left ㅠㅠ!!.");
+});
+
+socket.on("new_message", addMessage);
--- src/server.js
+++ src/server.js
@@ -10,13 +10,14 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req, res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 wsServer.on("connection", (socket) => {
     socket.onAny((event) => {
@@ -24,12 +25,23 @@ wsServer.on("connection", (socket) => {
     });
     // console.log(socket);
     socket.on("enter_room", (roomName, done) => {
+        // console.log(socket.rooms);
         socket.join(roomName);
-
-        setTimeout(()=>{
-            done("hello");
-        }, 15000);
+        // console.log(socket.rooms);
+        done();
+        socket.to(roomName).emit("welcome");
     });
+    socket.on("disconnecting", ()=>{
+        socket.rooms.forEach((room) => socket.to(room).emit("bye"));
+            
+        });
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
+
+    });    
+
 });
+
 
 httpServer.listen(3000, handleListen);
